33361e8713d3fac559eb471eddb5d467b680265d
闹钟大魔王 <1348651580@qq.com>
2023-02-02 14:33:41 +0800

    fix error: can't find font file

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 use lazy_static::lazy_static;
 
 pub const DEFAULT_WIN_WIDTH: u32 = 500;
@@ -5,9 +7,11 @@ pub const DEFAULT_WIN_HEIGHT: u32 = 800;
 pub const TITLE: &str = "人生重开模拟器";
 
 lazy_static! {
-    pub static ref DEFAULT_FONT: Vec<u8> = Vec::from(
-        include_bytes!("/home/yjn/.local/share/fonts/SourceHanSansCN-Regular.otf").as_slice()
-    );
+    pub static ref DEFAULT_FONT: Vec<u8> = fs::read::<String>(format!(
+        "{}/resource/SourceHanSansCN-Regular.otf",
+        env!("CARGO_MANIFEST_DIR")
+    ))
+    .expect("get font resouce failed");
     pub static ref TALENTS_DATA_PATH: String = {
         println!("CARGO_MANIFEST_DIR={}", env!("CARGO_MANIFEST_DIR"));
         format!("{}/data/zh-cn/talents.xlsx", env!("CARGO_MANIFEST_DIR"))
